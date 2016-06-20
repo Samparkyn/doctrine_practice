c9a4aedd67882c07051fc7d85c3e5444013b82be
Samparkyn <samparkyn101@gmail.com>
2016-06-20 15:52:34 +0100

    remove hard coded data and update twig files

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -50,26 +50,28 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
-        $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
+        $em = $this->getDoctrine()->getManager();
+        
+        $genus = $em->getRepository('AppBundle:Genus')
+            ->findOneBy(['name' => $genusName]);
 
-        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
-        $key = md5($funFact);
-        if ($cache->contains($key)) {
-            $funFact = $cache->fetch($key);
-        } else {
-            sleep(1); // fake how slow this could be
-            $funFact = $this->get('markdown.parser')
-                ->transform($funFact);
-            $cache->save($key, $funFact);
-        }
+        // $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+        // $key = md5($funFact);
+        // if ($cache->contains($key)) {
+        //     $funFact = $cache->fetch($key);
+        // } else {
+        //     sleep(1); // fake how slow this could be
+        //     $funFact = $this->get('markdown.parser')
+        //         ->transform($funFact);
+        //     $cache->save($key, $funFact);
+        // }
 
         $this->get('logger')
             ->info('Showing genus: '.$genusName);
 
-        return $this->render('genus/show.html.twig', array(
-            'name' => $genusName,
-            'funFact' => $funFact,
-        ));
+            return $this->render('genus/show.html.twig', array(
+                  'genus' => $genus
+              ));
     }
 
     /**
