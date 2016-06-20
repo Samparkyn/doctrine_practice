19813c44ee2ccc14fa6d77723b76167c92f607be
Samparkyn <samparkyn101@gmail.com>
2016-06-20 16:21:48 +0100

    create fixtures.yml to set values for dummy data

--- src/AppBundle/DataFixtures/ORM/LoadFixtures.php
+++ src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -11,11 +11,6 @@ class LoadFixtures implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $genus = new Genus();
-        $genus->setName('Octopus'.rand(1, 100));
-        $genus->setSubFamily('Octopodinae');
-        $genus->setSpeciesCount(rand(100, 99999));
-        $manager->persist($genus);
-        $manager->flush();
+       $objects = Fixtures::load(__DIR__.'/fixtures.yml', $manager);
     }
 }
