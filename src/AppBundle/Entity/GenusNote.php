03ee167195fc34fe6002f3ada32e9e33275603c3
Samparkyn <samparkyn101@gmail.com>
2016-06-21 09:51:57 +0100

    add GenusNote entity & its properties

---
+++ src/AppBundle/Entity/GenusNote.php
@@ -0,0 +1,41 @@
+<? php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="genus_note")
+ */
+class GenusNote {
+  
+  /**
+    * @ORM\Id
+    * @ORM\GeneratedValue(strategy="AUTO")
+    * @ORM\Column(type="integer")
+    */
+  private $id;
+  
+  /**
+    * @ORM\Column(type="string")
+    */
+  private $username;
+  
+  /**
+   * @ORM\Column(type="string")
+   */
+  private $userAvatarFilename;
+  
+  /**
+     * @ORM\Column(type="text")
+     */
+  private $note;
+  
+  /**
+     * @ORM\Column(type="datetime")
+     */
+  private $createdAt;
+  
+}
